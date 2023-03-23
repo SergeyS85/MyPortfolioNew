63bbcf100afa19361efe20b612c3ebd633fa5788
SergeyS85 <surzhiks.85@mail.ru>
2023-03-23 21:17:59 +0300

    delete files

--- progects/Ilon Mask/js/burgerMenu.js
+++
@@ -1,6 +0,0 @@
-let adaptiveMenu = document.querySelector('.container_adaptive-menu');
-let burger       = document.querySelector('.burger_btn');
-
-burger.addEventListener('click',function(){
-	adaptiveMenu.classList.toggle('is-active')
-})
