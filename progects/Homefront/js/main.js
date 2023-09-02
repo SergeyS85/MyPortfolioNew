7275454a74d5d484085ac9ff3fd3a8cc7dcb7e71
SergeyS85 <surzhiks.85@mail.ru>
2023-09-02 21:58:17 +0300

    complete header

--- progects/Homefront/js/main.js
+++ progects/Homefront/js/main.js
@@ -1,9 +1,17 @@
-
+// Функция для раскрытия списка языка в разделе headers
 let droplist       = document.querySelector('.lang');
-let droplistLi       = document.querySelectorAll('.lang li');
+let droplistLi     = document.querySelectorAll('.lang li');
+let displayLang    = document.querySelector('.display-lang')
+let langBlock      = document.querySelector('.lang__block')
 
 droplist.addEventListener('click',function(event){		
 			this.classList.toggle('plusminus__onoff');
-			console.log(this)		
+
+			// Функция для выбора языка в разделе headers
+      displayLang.innerText = event.target.innerText;
+      langBlock.style.background = 'url(images/headers/flags_lang/'+event.target.id+'.png) no-repeat';
+      langBlock.style.backgroundPosition = '0 center';
+      console.log(event.target.style)		
 })
+
 
