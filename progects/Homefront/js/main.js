bc3fcd96caa2d05a3d358b370f8f000a35597633
SergeyS85 <surzhiks.85@mail.ru>
2023-09-15 22:13:35 +0300

    complete homefront bottom block

--- progects/Homefront/js/main.js
+++ progects/Homefront/js/main.js
@@ -1,17 +1,24 @@
 // Функция для раскрытия списка языка в разделе headers
 let droplist       = document.querySelector('.lang');
-let droplistLi     = document.querySelectorAll('.lang li');
 let displayLang    = document.querySelector('.display-lang')
-let langBlock      = document.querySelector('.lang__block')
+let langBlock      = document.querySelector('.lang__block .lang__icon')
 
 droplist.addEventListener('click',function(event){		
 			this.classList.toggle('plusminus__onoff');
 
 			// Функция для выбора языка в разделе headers
-      displayLang.innerText = event.target.innerText;
-      langBlock.style.background = 'url(images/headers/flags_lang/'+event.target.id+'.png) no-repeat';
-      langBlock.style.backgroundPosition = '0 center';
-      console.log(event.target.style)		
+      
+      if(event.target.classList == 'language__li'){
+        // Добавляет выбранный язык в тег li.display-lang
+      	displayLang.innerText = event.target.innerText;
+        
+        // Меняет иконку флага выбранного языка
+      	langBlock.style.background = 'url(images/headers/flags_lang/'+event.target.id+'.png) no-repeat';
+
+      	// Позиционирует иконку флага
+        langBlock.style.backgroundPosition = 'center center';
+        displayLang.id = event.target.id
+      }	
 })
 
 
