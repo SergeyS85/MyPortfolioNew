5b51d074b2c8806146537ba0d1dd258bff9e5d3c
SergeyS85 <surzhiks.85@mail.ru>
2023-10-06 17:15:58 +0300

    complete developer section

--- progects/Homefront/js/main.js
+++ progects/Homefront/js/main.js
@@ -1,7 +1,10 @@
 // Функция для раскрытия списка языка в разделе headers
-let droplist       = document.querySelector('.lang');
-let displayLang    = document.querySelector('.display-lang')
-let langBlock      = document.querySelector('.lang__block .lang__icon')
+let droplist           = document.querySelector('.lang');
+let displayLang        = document.querySelector('.display-lang');
+let langBlock          = document.querySelector('.lang__block .lang__icon');
+let developerFotoBlock = document.querySelector('.developer__foto-block');
+let developersnameContainerP = document.querySelector('.developersname__container p');
+let developersnameContainerPSpan = document.querySelector('.developersname__container span');
 
 droplist.addEventListener('click',function(event){		
 			this.classList.toggle('plusminus__onoff');
@@ -18,7 +21,29 @@ droplist.addEventListener('click',function(event){
       	// Позиционирует иконку флага
         langBlock.style.backgroundPosition = 'center center';
         displayLang.id = event.target.id
-      }	
+      }
+
 })
 
 
+// Функция движения слайдера
+
+let sliderContainer = document.querySelector('.slider__container');
+let developerSliderBlock = document.querySelector('.developer__slider-block');
+let sliderCount = 0;
+sliderContainer.addEventListener('click', function(event){
+	if(event.target.classList == 'developer__arrow__left-block'){
+		sliderCount += 100;
+    developerSliderBlock.style.transform = 'translateX(-'+ sliderCount +'px)';
+	}
+
+	if(event.target.classList == 'developer__arrow__right-block'){
+		sliderCount -= 100;
+    developerSliderBlock.style.transform = 'translateX(-'+ sliderCount +'px)';
+	}
+
+	if(event.target.tagName == 'IMG'){
+  	developersnameContainerP.innerText = event.target.dataset.developername;
+  	developersnameContainerPSpan.innerText = event.target.dataset.professional;
+  }
+})
